db3a7426832f3a946ff1df35779e7285db74939a
paulczar <paul@paulcz.net>
2014-07-27 14:49:36 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ maintainer_email 'username.taken@gmail.com'
 license          'Apache2'
 description      'Installs/Configures pleaserun'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
